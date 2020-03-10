02dcae0dab971e146dcfb7f9f67d33e13c8f6a4a
Jery <oshindeveloper12@gmail.com>
2020-03-10 12:16:49 -0400

    this is a test using Gatsby Js

---
+++ src/pages/blog.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BlogPage = () =>{
+    return(
+        <div>
+            <h1>Blog</h1>
+            <p>Post will show up here later on</p>
+        </div>
+    )
+}
+export default BlogPage
+
+/*
+GOAL: CREATE AN ABOUT PAGE AND A CONTACT PAGE
+
+1. Create an about page. Include a page title and bio.
+2. Create a contact page. Include a page title and contact details.
+3. Test your work
+*/
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndexPage = () => {
+    return (
+        <div>
+            <h1> Hello </h1>
+            <h2> Working with Gatsby Js</h2>
+        </div>
+    );
+}
+
+export default IndexPage
