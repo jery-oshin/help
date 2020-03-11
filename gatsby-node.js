317fc1b386cbb9b37d64da5df30f95f6f450c9b6
Jery <oshindeveloper12@gmail.com>
2020-03-11 16:07:36 -0400

    Page templete for post are created, it is posible create new dinamic pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,5 +12,49 @@ module.exports.onCreateNode = ({node, actions }) => {
             value: slug
         })
     }
-
 }
+
+
+module.exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions;
+
+    const blogTemplate = path.resolve('./src/templates/blog.js');
+
+    const res = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
+    res.data.allMarkdownRemark.edges.forEach( (edge) => {
+        createPage({
+            component: blogTemplate,
+            path: `/blog/${edge.node.fields.slug}`,
+            context: {
+                slug: edge.node.fields.slug
+            }
+        })
+    })
+
+    // 1. Get path to template
+    // 2. Get markdown data
+    // 3. Create new pages
+}
+
+/* 
+GOAL 1 - Generate a slug for each post
+
+gatsby.md -> gatsby -> /blog/gatsby
+
+GOAL 2 - Generate the blog post page template
+
+GOAL 3 - Generate a new page for each post
+*/
---
+++ src/templates/blog.js
@@ -0,0 +1,16 @@
+//react components for templates
+
+import React from 'react';
+
+import Layout from '../components/layout.js';
+
+const Blog = () => {
+    return (
+        <Layout>
+            THis is the blog template
+        </Layout>
+    )
+}
+
+
+export default Blog
