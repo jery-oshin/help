15955160aafa414873401f284f76e586a9643045
Jery <oshindeveloper12@gmail.com>
2020-03-13 10:29:10 -0400

    Challege completed - Render Contentful Posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -11,18 +11,19 @@ const BlogPage = () =>{
     const data = useStaticQuery(graphql`
 
         query {
-            allMarkdownRemark {
-                edges {
-                    node {
-                        frontmatter {
-                            title
-                            date
-                        }
-                        fields {
-                            slug
-                        }
-                    }
+            allContentfulBlogPost (
+            sort: {
+                fields: publishedDate,
+                order: ASC
+            }
+            ) {
+            edges {
+                node {
+                title
+                slug
+                publishedDate (formatString:"MMM Do, YYYY")
                 }
+            }
             }
         }
 
@@ -34,12 +35,12 @@ const BlogPage = () =>{
         <Layout>
             <h1>Blog</h1>
             <ol className={blogStyles.posts}>
-                {data.allMarkdownRemark.edges.map( (edge)=> {
+                {data.allContentfulBlogPost.edges.map( (edge)=> {
                     return (
                         <li className={blogStyles.post}>
-                            <Link to={`/blog/${edge.node.fields.slug}`}>
-                                <h2>  {edge.node.frontmatter.title}</h2>
-                                <p> {edge.node.frontmatter.date}</p>
+                            <Link to={`/blog/${edge.node.slug}`}>
+                                <h2>  {edge.node.title}</h2>
+                                <p> {edge.node.publishedDate}</p>
                             </Link>
                             
                         </li>
