13bf7f38c8b7f5166217536e38b30af1160088a7
Jery <oshindeveloper12@gmail.com>
2020-03-13 09:07:06 -0400

    contentful plugin, configuration and .env.development added

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
     author: 'Jery Ramirez'
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      }
+    },
     'gatsby-plugin-sass', 
     {
       resolve: 'gatsby-source-filesystem',
@@ -28,7 +35,6 @@ module.exports = {
         ]
       }
     }
-    
   ]
 }
 
